80f48239080d0a6fdf58182917bbd55ba0e48ac5
lihsinchien <136952596+lihsinchien@users.noreply.github.com>
2023-06-18 14:00:34 +0800

    Add files via upload

---
+++ 無標題.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  無標題.py
+#  
+#  Copyright 2023 User <User@DESKTOP-P5KDEUL>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+print("kkk")
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+
+print(message)
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+message = "Hello, " + full_name + "!"
+print(message)
